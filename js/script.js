dcae0ed48d56f2628d2f3515a41c9d6cd53ad0dd
Matteo <grecomatteo86@gmail.com>
2021-03-05 17:11:48 +0100

    aggiunto evento al click del bottone

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var calcolo = document.getElementById('calcola');
+// console.log(calcolo);
+
+//1. aggiungo l'evento al click del bottone
+calcolo.addEventListener('click',
+
+  function(){
+
+    alert('bravo, il click funziona');
+
+  }
+
+);
