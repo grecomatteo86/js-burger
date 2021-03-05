67d5727f08e0fe9fd2b8c28c20dfa21d85bd574f
Matteo <grecomatteo86@gmail.com>
2021-03-05 17:45:11 +0100

    controllo il value del panino

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
+// 1. definisco le variabili che andremop ad utilizzare nel nostro programma
 var calcolo = document.getElementById('calcola');
 // console.log(calcolo);
 
-//1. aggiungo l'evento al click del bottone
+//2. aggiungo l'evento al click del bottone
 calcolo.addEventListener('click',
 
   function(){
 
-    alert('bravo, il click funziona');
+    // alert('bravo, il click funziona');
+
+    /*3. controllo se il nome del panino è stato inserito
+    (dobbiamo andarci a leggere l'input 29)*/
+    var nomePanino = document.getElementById('nome_panino');
+    /* di questo input devo andare a controllare il value
+    console.log(nomePanino.value);*/
+    /*4. eseguo il controllo:
+    se il valore del nome_panino è diverso da una stringa vuota allora voglio vederne il valore;
+    altrimenti compare un messaggio che mi chiede di inserire il nome del panino*/
+    if (nomePanino.value != '') {
+      console.log(nomePanino.value);
+    } else {
+      alert('inserisci il nome del panino');
+    }
 
   }
 
