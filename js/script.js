0b3642a2c1b9040b441f7fce186ecd10e38e6dd9
Matteo <grecomatteo86@gmail.com>
2021-07-21 12:26:38 +0200

    fixing ingredients list

--- js/script.js
+++ js/script.js
@@ -13,14 +13,14 @@ calcolo.addEventListener('click',
 
     /*3. controllo se il nome del panino è stato inserito
     (dobbiamo andarci a leggere l'input 29)*/
-    var nomePanino = document.getElementById('nome_panino');
+    var burgerName = document.getElementById('nome_panino');
     /* di questo input devo andare a controllare il value
-    console.log(nomePanino.value);*/
+    console.log(burgerName.value);*/
     /*4. eseguo il controllo:
     se il valore del nome_panino è diverso da una stringa vuota allora voglio vederne il valore;
     altrimenti compare un messaggio che mi chiede di inserire il nome del panino*/
-    if (nomePanino.value != '') {
-      /* console.log(nomePanino.value);
+    if (burgerName.value != '') {
+      /* console.log(burgerName.value);
       5. ad ogni click devo andare a scorrere tutte le checkbox
       per vedere se sono checkate o no;
       per far questo vado a prendere tutte le checkbox attraverso la classe
